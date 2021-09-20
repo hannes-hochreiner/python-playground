cce1924e57eeaf5a4ef8c0ce7d8fe5ac12233b6a
Hannes Hochreiner <hannes@hochreiner.net>
2021-09-20 20:16:15 +0200

    switch to ternary operator

--- factorial.py
+++ factorial.py
@@ -1,8 +1,5 @@
 def factorial(num):
-  if num == 0:
-    return 1
-  else:
-    return num * factorial(num - 1)
+  return 1 if num == 0 else num * factorial(num-1)
 
 number = int(input("Please enter a number: "))
 print(f"The factorial of {number} is {factorial(number)}")
